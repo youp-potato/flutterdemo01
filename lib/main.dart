cda1adebb78c7a81c27bd16b80973c97fa9dccb0
hx <hx1187390397@163.com>
2023-08-24 16:33:43 +0800

    第22节14分

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,9 @@ void main(){
       body: const Column( //  通过Column加载多个组件
         children: [
           MyApp(),
-          MyButton()
+          MyButton(),
+          MyText(),
+          MyImage()
         ],
       )
     ),
@@ -23,9 +25,13 @@ class MyApp extends StatelessWidget{
     // TODO: implement build
     return Center(
       child: Container(//Container不是常量构造函数
+        margin: const EdgeInsets.fromLTRB(0, 60, 0, 0),
         alignment: Alignment.center,
         width: 100,
         height: 100,
+        // transform: Matrix4.translationValues(10, 0, 0), // 位移  //通过transform可以使Container容易进行一些旋转、平移、倾斜的操作
+        // transform: Matrix4.rotationZ(0.2),  //旋转
+        transform: Matrix4.skewY(0.2),  //倾斜
         decoration: BoxDecoration(  //BosDecoration是可装饰的decoration
           color: Colors.red, //背景颜色
           border: Border.all( //配置背景边框
@@ -47,9 +53,9 @@ class MyApp extends StatelessWidget{
             ]
           )
         ),
-        child: const Text("Hello Flutter",style: TextStyle(
+        child: const Text("你好 Flutter",style: TextStyle(
           color: Colors.white,
-          fontSize: 20
+          fontSize: 15
         ),),
       )//const表示多个相同的实例共享存储空间
     );
@@ -57,7 +63,6 @@ class MyApp extends StatelessWidget{
 }
 
 class MyButton extends StatelessWidget{ //通过Container创建按钮
-  // const MyButton({super.key});
   const MyButton({Key? key}): super(key: key);
 
   @override
@@ -70,6 +75,7 @@ class MyButton extends StatelessWidget{ //通过Container创建按钮
       //padding是让容器和里面的元素有相应的距离，margin是让容器和容器外部的其他容器有相应的间距
       // margin: const EdgeInsets.all(10),  //表示四周margin
       margin: const EdgeInsets.fromLTRB(0, 40, 0, 0), //表示固定方向间隔距离
+      // padding: const EdgeInsets.fromLTRB(40, 0, 0, 0),
       decoration: BoxDecoration(
         color: Colors.blue,
         borderRadius: BorderRadius.circular(10) //设置按钮边框圆角
@@ -80,4 +86,60 @@ class MyButton extends StatelessWidget{ //通过Container创建按钮
     );
   }
 
+}
+
+class MyText extends StatelessWidget{
+  const MyText({Key? key}): super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      width: 200,
+      height: 100,
+      margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+      decoration: const BoxDecoration(
+        color: Colors.yellow
+      ),
+      child: const Text("你好我是flutter你好我是flutter你好我是flutter你好我是flutter你好我是flutter",
+        textAlign: TextAlign.left,
+        overflow: TextOverflow.ellipsis,  //文字内容溢出显示几个点...
+        maxLines: 1,//表示显示几行文字
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w900,//字体粗细
+          color: Colors.red,
+          fontStyle: FontStyle.italic, //表示文字样式，italic斜体 normal正常
+          letterSpacing: 2,  //字间距
+          decoration: TextDecoration.underline,
+          decorationColor: Colors.black,
+          decorationStyle: TextDecorationStyle.dashed //表示虚线
+        ),),
+    );
+  }
+
+}
+
+class MyImage extends StatelessWidget{  //图片显示
+  const MyImage({Key? key}): super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      width: 350,
+      height: 250,
+      margin: const EdgeInsets.fromLTRB(0, 45, 0, 0),
+      decoration: const BoxDecoration(
+        color: Colors.yellow
+      ),
+      child: Image.network("https://lmg.jj20.com/up/allimg/4k/s/02/2109250006343S5-0-lp.jpg",
+        // scale: 2,//表示图片缩小一倍
+        // alignment: Alignment.centerLeft, //控制图片的位置
+        // fit: BoxFit.fill, //在容器全屏显示
+        // fit: BoxFit.cover,  //对图片进行剪裁不压缩图片
+        // repeat: ImageRepeat.repeatX,  //在x轴进行平铺
+        repeat: ImageRepeat.repeat,  //在x、y轴都进行平铺
+      ),
+    );
+  }
+  
 }
